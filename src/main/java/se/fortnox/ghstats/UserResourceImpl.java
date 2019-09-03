3298a9c2bb45a02d615323dbd920f6e8bbf21fc8
Magnus Persson <magnus.e.persson@gmail.com>
2019-09-03 14:36:01 +0200

    Tracing

--- src/main/java/se/fortnox/ghstats/UserResourceImpl.java
+++ src/main/java/se/fortnox/ghstats/UserResourceImpl.java
@@ -1,56 +1,65 @@
 package se.fortnox.ghstats;
 
-
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
+import se.fortnox.ghstats.github.GithubRepo;
 import se.fortnox.ghstats.github.GithubResource;
 import se.fortnox.ghstats.github.GithubUser;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @RestController
 public class UserResourceImpl implements UserResource {
-    private       GithubResource  githubResource;
-    private final Tracer tracer;
+	private       GithubResource githubResource;
+	private final Tracer         tracer;
 
-    @Autowired
-    public UserResourceImpl(GithubResource githubResource, Tracer tracer) {
-        this.githubResource = githubResource;
-        this.tracer = tracer;
-    }
+	private static <T> Consumer<Signal<T>> logOnNext(Function<T, String> logStatement) {
+		return signal -> {
+			if (!signal.isOnNext()) return;
+			Optional<Span> maybeSpan = signal.getContext().getOrEmpty(Span.class);
 
-    @Override
-    public Mono<GithubUser> describe(String username) {
-        return Mono.zip(githubResource.fetchUser(username), githubResource.fetchRepos(username), (githubUser, githubRepos) -> {
-            githubRepos.forEach(githubRepo -> {
-                githubUser.getRepos().add(githubRepo.getName());
-            });
-            return githubUser;
-        }).flatMap(githubUser -> {
-            return Mono.subscriberContext().flatMap(context -> {
-                Span span = context.getOrDefault(Span.class, null);
-                if(span == null) {
-                    return Mono.just(githubUser);
-                }
-                Span userspace = tracer.buildSpan("userspace").asChildOf(span).start();
-                userspace.log("Some user data");
-                return Mono.just(githubUser).doOnTerminate(userspace::finish);
-            });
-        });
-    }
+			maybeSpan.ifPresent(span -> span.log(logStatement.apply(signal.get())));
+		};
+	}
 
-    @Override
-    public Flux<GithubUser> stream() {
-        List<Mono<GithubUser>> fluxes = List.of(
-                githubResource.fetchUser("magnusp"),
-                githubResource.fetchUser("magnusp").delaySubscription(Duration.of(5, ChronoUnit.SECONDS))
-        );
-        return Flux.concat(fluxes);
-    }
+	@Autowired
+	public UserResourceImpl(GithubResource githubResource, Tracer tracer) {
+		this.githubResource = githubResource;
+		this.tracer = tracer;
+	}
+
+	@Override
+	public Mono<GithubUser> describe(String username) {
+		Mono<GithubUser> userObservable = githubResource
+			.fetchUser(username)
+			.doOnEach(logOnNext(githubUser -> String.format("Fetched user %s", githubUser.getName())));
+
+		Mono<List<GithubRepo>> repoObservable = githubResource
+			.fetchRepos(username)
+			.doOnEach(logOnNext(githubRepos -> String.format("Fetched %d repos", githubRepos.size())));
+
+		return Mono.zip(userObservable, repoObservable, (githubUser, githubRepos) -> {
+			githubRepos.forEach(githubRepo -> githubUser.getRepos().add(githubRepo.getName()));
+			return githubUser;
+		});
+	}
+
+	@Override
+	public Flux<GithubUser> stream() {
+		List<Mono<GithubUser>> fluxes = List.of(
+			githubResource.fetchUser("magnusp"),
+			githubResource.fetchUser("magnusp").delaySubscription(Duration.of(5, ChronoUnit.SECONDS))
+		);
+		return Flux.concat(fluxes);
+	}
 }
